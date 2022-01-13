7f5dc193d49eae0a576ced178149b13ade287788
Sayak Kundu <sayakk1998@gmail.com>
2022-01-13 16:25:52 +0530

    new java file added

---
+++ Sum.java
@@ -0,0 +1,13 @@
+class Sum
+{
+    public static void main(String[] args) 
+    {
+        int sum = 0;
+        int a = 6;
+        int b = 7;
+
+        sum = a + b;
+        System.out.println("SUM: "+sum);
+
+    }
+}
